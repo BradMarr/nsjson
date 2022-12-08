845a6d266fbda7070dcd68d6db00c99410efca01
BradMarr <108196186+BradMarr@users.noreply.github.com>
2022-12-08 01:05:23 -0500

    Updated Comments and Added Trailing Comma Support

--- nsjson/Program.cs
+++ nsjson/Program.cs
@@ -1,7 +1,5 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 
-string? input = null;
-string[]? inputArray = {"",""};
 string TerminalURL(string caption, string url) => $"\u001B]8;;{url}\a{caption}\u001B]8;;\a";
 
 Console.WriteLine("      ___          ___           ___       ___          ___          ___     \n" +
@@ -16,11 +14,15 @@ Console.WriteLine("      ___          ___           ___       ___          ___  
                              "     /:/  /      \\::/  /                 \\::/  /      \\::/  /       /:/  /   \n" +
                              "     \\/__/        \\/__/                   \\/__/        \\/__/        \\/__/    \n");
 
-try
+void runtime()
 {
+    Console.ForegroundColor = ConsoleColor.White;
+    string? input = null;
+    string[]? inputArray = {""};
+
     while (true)
     {
-        if (inputArray[0] == "compile" && inputArray.Length > 1)
+        if (inputArray[0] == "compile")
         {
             string[] linesArray = File.ReadAllLines(@inputArray[1]);
 
@@ -30,16 +32,42 @@ try
             }
             var outFile = File.CreateText(inputArray[1] + ".json");
 
-            foreach (string line in linesArray) 
-            { 
-                string[] lineArray = line.Split("//"); 
-                outFile.WriteLine(lineArray[0]); 
-            }
+            int linesIndex = 0;
+            foreach (string line in linesArray)
+            {
+                string finalLine;
+                char[] letterArray = line.ToCharArray();
+                int letterIndex = 0;
+                foreach (char letter in letterArray)
+                {
+                    if (letterIndex < letterArray.Length-1)
+                    {
+                        if (letterArray[letterIndex] == ',' && letterArray[letterIndex + 1] == '}' || letterArray[letterIndex] == ',' && letterArray[letterIndex + 1] == ']')
+                        {
+                            letterArray[letterIndex] = ' ';
+                        };
+                    }
+                    else
+                    {
+                        if (letterArray[letterIndex] == ',' && linesArray[linesIndex+1].ToCharArray()[0] == '}' || letterArray[letterIndex] == ',' && linesArray[linesIndex+1].ToCharArray()[0] == ']')
+                        {
+                            letterArray[letterIndex] = ' ';
+                        }
+                    }
+
+                    letterIndex++;
+                };
+
+                finalLine = string.Concat(letterArray);
+                linesArray[linesIndex] = finalLine;
+                string[] lineArray = linesArray[linesIndex].Split("//note//");
+
+                outFile.WriteLine(lineArray[0]);
+                linesIndex++;
+            };
             outFile.Close();
-
-
-
-            Console.Write(" $");
+            Console.WriteLine(" compiled.");
+            Console.Write(" $ ");
         }
 
 
@@ -53,25 +81,34 @@ try
             {
                 Console.WriteLine(TerminalURL("https://docs.blubrad.com/nsjson", "https://docs.blubrad.com/nsjson"));
             }
-            Console.Write(" $");
+            Console.Write(" $ ");
         }
         else             //help
         {
-            Console.WriteLine(" ┌─────────┬──────────────┬──────────────────────────────────────┐\n" +
-                              " │ command │ arguments    │ description                          │\n" +
-                              " ├─────────┼──────────────┼──────────────────────────────────────┤\n" +
-                              " │ help    │ -            │ shows this message                   │\n" +
-                              " │ docs    │ -            │ displays documentation page          │\n" +
-                              " │ compile │ <path>       │ compiles the file the path points to │\n" +
-                              " └─────────┴──────────────┴──────────────────────────────────────┘");
-            Console.Write(" $");
+            Console.WriteLine(" ┌─────────┬───────────────────┬──────────────────────────────────────┐\n" +
+                              " │ command │ arguments         │ description                          │\n" +
+                              " ├─────────┼───────────────────┼──────────────────────────────────────┤\n" +
+                              " │ help    │ -                 │ shows this message                   │\n" +
+                              " │ docs    │ -                 │ displays documentation page          │\n" +
+                              " │ compile │ <path>            │ READ DOCS FIRST!!                    │\n" +
+                              " └─────────┴───────────────────┴──────────────────────────────────────┘");
+            Console.Write(" $ ");
         }
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
 
         input = Console.ReadLine();
-        inputArray = input.Split(" ");
-    } 
+        inputArray = input?.Split(" ");
+    }
+}
+
+try
+{
+    runtime();
 }
 catch (Exception e)
 {
-    Console.WriteLine(e);
+    Console.ForegroundColor = ConsoleColor.Red;
+
+    Console.WriteLine(e.ToString(), Console.ForegroundColor);
+    runtime();
 }
